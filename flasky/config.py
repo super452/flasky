eed529e6f100fc6713dff2120a6d2e1925f6869f
super <443027458@qq.com>
2016-03-01 22:08:08 +0800

    first blood

--- flasky/config.py
+++ flasky/config.py
@@ -38,6 +38,14 @@ class ProductionConfig(Config):
     def init_app(cls, app):
         Config.init_app(app)
 
+        #  log to stderr
+        import logging
+        from logging import StreamHandler
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(file_handler)
+
+
         #email errors to the administrators
         import logging
         from logging.handlers import SMTPHandler
@@ -56,6 +64,12 @@ class ProductionConfig(Config):
             secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+
+class HerokuConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
 
 config = {
     'development': DevelopmentConfig,
